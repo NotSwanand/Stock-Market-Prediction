88ae0683143043225813b539ea5c0cd952b6a927
NotSwanand <swanand264@gmail.com>
2025-09-21 20:43:06 +0530

    main.py update

--- main.py
+++ main.py
@@ -323,4 +323,6 @@ def insertintotable():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
